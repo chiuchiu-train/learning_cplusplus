515a9bb60c0bbfb9c553db4827dbdfb408e45c30
Katherine Chiu <kachiu@ucdavis.edu>
2022-05-13 13:15:03 -0700

    need to try associative array

---
+++ rosalind/REVC.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <string>
+
+int main(){
+    std::string DNA; // initialize a string
+    std::cout << "Input a DNA string: "; // ask for user input
+    std::cin >> DNA; // assign user input to the variable DNA
+
+    // std::cout << DNA.length() << std::endl; // find length of DNA seq
+
+    // Reverse the DNA string
+    std::string reversed; // to store the reversed string
+    for (int i=DNA.length()-1; i>=0; i--){ // reverse index DNA string
+        reversed += DNA[i];
+    }
+    std::cout << "Reversed: " << reversed << std::endl;
+
+    std::string revc; // to store reverse complement
+    for (auto i:reversed){
+        if(i=='A'){
+            revc += 'T';
+        }
+        if(i=='C'){
+            revc += 'G';
+        }
+        if(i=='T'){
+            revc += 'A';
+        }
+        if(i=='G'){
+            revc += 'C';
+        }
+    }
+    std::cout << "Reverse complement :" << revc << std::endl;
+
+    return 0;
+}
