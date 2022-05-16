ae74d02798e666d84c1c7dc207bd02ec73c34be5
Katherine Chiu <kachiu@ucdavis.edu>
2022-05-16 14:32:15 -0700

    notes

---
+++ basics/numberSystems.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main(){
+    int number1 = 15; // decimal
+    int number2 = 017; //octal
+    int number3 = 0x0f; // hexadecimal
+    int number4 = 0b00001111; // binary
+
+    std::cout << number1 << std::endl;
+    std::cout << number2 << std::endl;
+    std::cout << number3 << std::endl;
+    std::cout << number4 << std::endl;
+}
---
+++ basics/types.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+int main(){
+    // INTEGER
+    int integr = 5;
+    std::cout << integr << std::endl;
+
+    // DOUBLE - represents a fractional value
+    double db = 2.9;
+    std::cout << db << std::endl;
+
+    // FLOAT - also represents a fractional value
+    float flt = 6.9;
+    std::cout << flt << std::endl;
+
+    // CHARACTER (char)
+    // char word = "string"; // chars are a single character enclosed in single quotes
+    char letter = 'A';
+    std::cout << letter << std::endl;
+
+    // BOOL
+
+    // VOID
+
+    // AUTO
+}
